2ee50596cc71b04699d2660ad060206008432c8c
awaysatw-lab <awaysatw@gmail.com>
2026-09-11 17:30:30 +0900

    20260911:question

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,13 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, url_for, redirect
+
 
 #blueprint 라우팅 함수를 체계적으로 관리
 bp=Blueprint('main', __name__, url_prefix='/')
 
 @bp.route('/')
-def hello_pybo():
-    return'Hello Pybo!'
-
-
-@bp.route('/hello')
-def hello():
-    return 'Hello Page!'
+def index():
+    return redirect(url_for('question._list'))
